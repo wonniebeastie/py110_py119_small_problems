9e0f7e0b8b783eeb80899bc5f23ccd7de68d42ae
Wonnie <wonnie.tech@gmail.com>
2025-08-25 13:30:00 -0700

    Solve problem manual way without f"{v:02d}"
    Wanted to see how it can be solved without using the f-string format
    specification. It was more trickier because fixing a couple of the test
    cases made them work but it made the other ones not work anymore.
    
    So had to figure out a more unified approach to get them all to be
    solved.
    
    Slowly figuring out a method of debugging that works for me.

--- easy_2/1_cute_angle.py
+++ easy_2/1_cute_angle.py
@@ -66,3 +66,79 @@ print(dms(254.6) == "254°35'59\"" or dms(254.6) == "254°36'00\"")
 print(dms(93.034773) == "93°02'05\"")
 print(dms(0) == "0°00'00\"")
 print(dms(360) == "360°00'00\"" or dms(360) == "0°00'00\"")
+
+
+"""
+SECOND APPROACH:
+If the f-string format specification can't be used for whatever reason:
+
+Algo:
+- initialize `min` with the result of (angle_in_float % 1) * 60
+- initialize `sec` with the result of (min % 1) * 60
+- return f-string of joined integer version of angle_in_float, the result of
+  pad_zeros(min), and the result of pad_zeros(sec)
+
+-- HELPER (`pad_zeros()`) --
+I: an integer, min or sec
+O: the input padded with another zero, as strings
+
+Algo:
+- turn input into an integer,
+- turn integer into a string
+- if length of the string is 1:
+    - concatenate "0" to the beginning of the string & return it
+- return it
+
+0 -> 0 -> "0" ->
+2.23493 -> 2 -> "2" ->
+35.654543 -> 35 -? "35" ->
+"""
+DEGREE = "\u00B0"
+
+def dms(angle_in_float):
+    min = (angle_in_float % 1) * 60
+    sec = (min % 1) * 60
+    return f"{int(angle_in_float)}{DEGREE}{pad_zeros(min)}'{pad_zeros(sec)}\""
+
+def pad_zeros(num):
+    num = str(int(num))
+    if len(num) == 1:
+        return "0" + num
+    return num
+
+"""
+DEBUGGING NOTES:
+
+Problem:
+- sec is not being calculated correctly due to the `int()` in min
+
+Options: [DID NOT WORK]
+- remove int from min 
+    - min will not be truncated
+        - Problem: meaning that the pad_zeros() will receive possibly 
+          a long int, and change it into a string
+            - Option: move the int() to inside pad_zeros()
+                - 2.0863800000000765 -> 2 -> "2"
+                - "0" + "2"
+                - "02"
+
+Problem: the zeros are covered because their length is 1, but
+the long floats are not being covered by the if conditions
+
+Problem: the test cases with non-zero numbers for both places are
+not being truncated properly.
+
+Option 1: [DID NOT WORK]
+- put int() for both min and sec in the return line 
+    - does not work because turning the strings into integers makes
+      them short again "00" -> 0
+
+Option 2: [WORKED!]
+- change pad_zeros function
+    - if input is 0: get padded with "0" in front
+    - if input is a single non-zero number (i.e. 2 or 5), then:
+        - truncated it to lose the decimal part
+        - also pad with "0" in front
+    - if input has non-zeros for both places, then just return the
+      integer version
+"""
