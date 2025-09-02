9f6c8fd523ff297fb721ea27731f8bcb690066b4
Wonnie <wonnie.tech@gmail.com>
2025-09-02 16:01:45 -0700

    Add problems 3 & 4 of Easy 3

--- easy_3/3_double_char_part1.py
+++ easy_3/3_double_char_part1.py
@@ -2,6 +2,46 @@
 Write a function that takes a string, doubles every character in the string, 
 then returns the result as a new string.
 """
+"""
+I: a string
+O: a new string, with each character doubled
+
+Breakdown:
+- initialize doubled_str to an empty string
+- Iterate through the string, and for each character:
+    - multiply it by 2
+    - concatenate it to doubled_str
+- return doubled_str
+"""
+# Using string concatenation
+def repeater(txt):
+    doubled_str = ''
+
+    for char in txt:
+        doubled_str += char * 2
+
+    return doubled_str
+
+"""
+Breakdown:
+- initialize doubled_str to an empty list
+- Iterate through the string, and for each character:
+    - multiply it by 2
+    - append it to doubled_str
+- join substrings in doubled_str together & return it
+"""
+# Using a list comprehension
+def repeater(txt):
+    return ''.join([char * 2 for char in txt])
+
+# Using a generator expression
+def repeater(txt):
+    return ''.join(char * 2 for char in txt)
+
+"""
+The generator expression is more memory-friendly.
+"""
+
 print(repeater('Hello') == "HHeelllloo")              # True
 print(repeater('Good job!') == "GGoooodd  jjoobb!!")  # True
 print(repeater('') == "")                             # True
--- easy_3/4_double_char_part2.py
+++ easy_3/4_double_char_part2.py
@@ -5,6 +5,52 @@ and returns the result as a new string. The function should not double vowels
 
 You may assume that only ASCII characters will be included in the argument.
 """
+"""
+I: a string
+O: a new string with every consonant in input string doubled
+
+Rules:
+- vowels, digits, punctuation, whitespace should be skipped
+
+Breakdown:
+- set CONSONANTS to 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+- iterate through the string, and for each character:
+    - if it exists in CONSONANTS:
+        - add char * 2 to list
+    - else:
+        - add char to list
+- join substrings together and return it
+"""
+CONSONANTS = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+
+# Using a list
+def double_consonants(txt):
+    doubled_cons = []
+
+    for char in txt:
+        if char in CONSONANTS:
+            doubled_cons.append(char * 2)
+        else:
+            doubled_cons.append(char)
+
+    return ''.join(doubled_cons)
+
+# Using a ternary expression in a generator expression
+def double_consonants(txt):
+    return ''.join((char * 2 if char in CONSONANTS else char) for char in txt)
+
+# Using string concatenation
+def double_consonants(txt):
+    doubled_cons = ''
+
+    for char in txt:
+        if char in CONSONANTS:
+            doubled_cons += char * 2
+        else:
+            doubled_cons += char
+
+    return doubled_cons
+
 # All of these examples should print True
 print(double_consonants('String') == "SSttrrinngg")
 print(double_consonants('Hello-World!') == "HHellllo-WWorrlldd!")
