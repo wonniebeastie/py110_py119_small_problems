a32182c9e668adc2af42c5e9911907480323620b
Wonnie <wonnie.tech@gmail.com>
2025-12-28 10:32:00 -0800

    Do number 4 of easy 4 exercises

--- easy_4/4_arrange_a_dictionary.py
+++ easy_4/4_arrange_a_dictionary.py
@@ -2,6 +2,27 @@
 Given a dictionary, return its keys sorted by the values associated with each 
 key.
 """
+"""
+I: a dict
+O: a list, sorted
+
+Ex:
+- {'p': 8, 'q': 2, 'r': 6} => ['q', 'r', 'p']
+
+Rules:
+- values dictate the order (numbers)
+- sort in ascending order
+- return only the keys as a list
+
+DS/Brainstorm:
+- sorted() -> returns a list
+    - sorts by key
+    - key= parameter
+        - get
+"""
+def order_by_value(dictionary):
+    return sorted(dictionary, key=dictionary.get)
+
 my_dict = {'p': 8, 'q': 2, 'r': 6}
 keys = ['q', 'r', 'p']
 print(order_by_value(my_dict) == keys)  # True
