9db54e7933ffb84e447b336f342576af139c8c84
Wonnie <wonnie.tech@gmail.com>
2025-12-30 09:59:36 -0800

    Add problem 5 from easy 4 exercises
    `-` is the operator version of the `.difference()` method.

--- easy_4/5_unique_elements.py
+++ easy_4/5_unique_elements.py
@@ -3,6 +3,9 @@ From two list arguments, determine the elements that are unique to the first
 list. The return value should be a set.
 
 """
+def unique_from_first(lst1, lst2):
+    return set(lst1) - set(lst2)
+
 list1 = [3, 6, 9, 12]
 list2 = [6, 12, 15, 18]
-print(unique_from_first(list1, list2) == {9, 3})
+print(unique_from_first(list1, list2) == {9, 3}) # True
